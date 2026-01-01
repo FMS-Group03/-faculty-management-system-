932adf767ccca0b94aa4b80a83b85d65cdf1382d
DilshanRathnayake <lahirudilshanr2003@gmail.com>
2026-01-02 01:47:37 +0530

    Added model and main integration

--- src/com/faculty/main/Main.java
+++ src/com/faculty/main/Main.java
@@ -1,4 +1,15 @@
 package com.faculty.main;
 
+
+import com.faculty.view.LoginView;
+
 public class Main {
+    public static void main(String[] args) {
+
+        LoginView loginView = new LoginView();
+
+
+
+
+    }
 }
