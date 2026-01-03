1ecf14424b78d055b07263f747573a599b33d656
thanoban <thanobansk@gmail.com>
2026-01-03 21:11:16 +0530

    admin part updated

--- src/com/faculty/main/Main.java
+++ src/com/faculty/main/Main.java
@@ -1,13 +1,17 @@
 package com.faculty.main;
 
+import com.faculty.controller.AdminController;
 import com.faculty.controller.LoginController;
+import com.faculty.view.AdminDashboardView;
 import com.faculty.view.LoginView;
 
 public class Main {
     public static void main(String[] args) {
 
-        LoginView view = new LoginView();
-
-        new LoginController(view);
+//        LoginView view = new LoginView();
+//
+//        new LoginController(view);
+        AdminDashboardView view = new AdminDashboardView();
+        new AdminController(view);
     }
 }
