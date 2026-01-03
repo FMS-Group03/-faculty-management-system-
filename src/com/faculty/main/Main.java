e9425a3aa27e3f41a9cb8eb6341a9715acf07656
KRLiyanage <kaweeshravishanka8@gmail.com>
2026-01-03 10:40:30 +0530

    First commit on CS_2022_036

--- src/com/faculty/main/Main.java
+++ src/com/faculty/main/Main.java
@@ -1,4 +1,9 @@
 package com.faculty.main;
 
+import com.faculty.view.LoginView;
 public class Main {
+    public static void main(String[] args) {
+        LoginView fram = new LoginView();
+        fram.setVisible(true);
+    }
 }
