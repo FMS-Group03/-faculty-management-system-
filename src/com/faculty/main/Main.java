b2a3413eb9fd89ac8acf8698c7cf335fa338071f
Dilshan Sandeepa <dilshansandeepa025@gmail.com>
2026-01-08 00:27:44 +0530

    Update Main.java
    Update

--- src/com/faculty/main/Main.java
+++ src/com/faculty/main/Main.java
@@ -5,48 +5,40 @@ import com.faculty.controller.LoginController;
 import com.faculty.view.LoginView;
 import com.faculty.view.AdminDashboardView;
 
-// me
 import com.faculty.view.StudentDashboardView;
 import com.faculty.controller.StudentController;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
- CS_2022_042
+
         SwingUtilities.invokeLater(() -> {
             LoginView view = new LoginView();
             new LoginController(view);
         });
 
-
-
-        //me
         SwingUtilities.invokeLater(() -> {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
             LoginView view = new LoginView();
             new LoginController(view);
             view.setVisible(true);
 
-//            to show save changes error & success
 //            StudentDashboardView vieww = new StudentDashboardView("Dilshan");
 //            new StudentController(vieww);
 //            vieww.setVisible(true);
 
-//            for admin view
 //            AdminDashboardView  View = new AdminDashboardView();
 //            new AdminController(View);
         });
+        
 //        LoginView view = new LoginView();
-//
 //        new LoginController(view);
+        
         AdminDashboardView view = new AdminDashboardView();
         new AdminController(view);
  
