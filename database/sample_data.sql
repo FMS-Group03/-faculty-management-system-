1913b99fded7f8e21e66939d18c52111579c348f
Lahiru Dilshan Rathnayake <lahirudilshanr2003@gmail.com>
2026-01-08 15:17:28 +0530

    Add sample data for students and lecturers

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -8,9 +8,31 @@ INSERT INTO `sign_up_table` (`username`, `password`, `role`) VALUES
 ('Dilshan', '1q2w3e4r', 'Student'),
 ('Dilshan', '1q2w3e4r', 'Student'),
 ('DilshanRathnayake', '1q2w3e4r5t', 'Student'),
-('rathnayake', '12345678', 'Lecturer');
-COMMIT;
+('rathnayake', '12345678', 'Lecturer'),
+('kavindu', '1q2w3easd', 'Student'),
+('Theshan', '1qa2ws3ed4rf', 'Lecturer');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+--
+-- Dumping data for table `lecture_course_details`
+--
+
+INSERT INTO `lecture_course_details` (`Lecture_Id`, `Course_Code`, `Course_Name`) VALUES
+('Theshan', '21042', 'Software Engineering'),
+('rathnayake', '31067', 'Cyber Security');
+
+
+
+
+--
+-- Dumping data for table `lecture_dashboard`
+--
+
+INSERT INTO `lecture_dashboard` (`Lecture_Id`, `Full_Name`, `Department`, `Lecture_Email`, `Lecture_Mobile_Number`) VALUES
+('Lec_2022_042', 'Dilshan Rathnayake', 'Cyber Security', 'rathnayake@gmail.com', '0715648724'),
+('SE_2022_082', 'Kavindu', 'software', 'kavindu@gmail.com', '0725364973');
